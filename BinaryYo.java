728983b7a54a1820e13745453e9e0750d6091ad7
Arshad <arshadquershy123456789@gmail.com>
2017-10-02 14:57:51 +0530

    improvement in decimal to binary program

--- BinaryYo.java
+++ BinaryYo.java
@@ -3,25 +3,36 @@ class BinaryYo
 {
 	public static long  DecimalToBinary(long n) {
 		long remainder;
-		long  binary=0,i=1;
+		long  binary=0,i=1,count=0;
 		while(n!=0){
-			remainder=n%2;
-			n=n/2;
-			binary=binary+(remainder*i);
-			i=i*10;		
-
+		 remainder=n%2;
+		 if(remainder==1)
+		   {
+			 count++;
+		   }
+		   n=n/2;
+		   binary=binary+(remainder*i);
+		   i=i*10;
+		}	
+			//System.out.println("the number of ones is:"+count);		
+		/*while(binary>0)
+			{
+				if (binary%10==1)
+				count++;
 		}
-		return binary;
+		System.out.println("the number of ones is:"+count);
+		*/
+	 System.out.println("the number of ones is :"+count);
+	 return binary;
+	} 
 
-		
-	}
 	public static void main(String[] args){
-			Scanner in=new Scanner(System.in);
-			long  decimal;
-
-		System.out.println("enter the number");
+		Scanner in=new Scanner(System.in);
+		long  decimal;
+        System.out.println("enter the number");
 		decimal=in.nextInt();
 		System.out.println(DecimalToBinary(decimal));
+		//System.out.println("the number of ones is:"+count);
 
 	}
 }
